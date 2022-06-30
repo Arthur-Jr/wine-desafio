c43d403d6906fe5df21e4bf23fb2ca7c5c8fb64f
Arthur-Jr <arthurjr1906@gmail.com>
2022-06-30 16:16:15 -0300

    feat: Add Header on index.tsx

--- src/components/header/style.tsx
+++ src/components/header/style.tsx
@@ -66,39 +66,6 @@ export const HamburguerButtonWrapper = styled.div`
   }
 `;
 
-export const ImageButtonWrapper = styled.a`
-  display: inherit;
-  width: 65px;
-  height: 60px;
-
-  &:nth-child(1) {
-    width: 40px;
-    height: 40px;
-  }
-
-  &:nth-child(2) {
-    display: none;
-  }
-
-  @media only screen and (min-width: 1000px) {
-    width: 56px;
-    height: 56px;
-
-    &:hover {
-      zoom: 1.1;
-    }
-
-    &:nth-child(1) {
-      width: 56px;
-      height: 56px;
-    }
-
-    &:nth-child(2) {
-      display: inherit;
-    }
-  }
-`;
-
 export const HeaderLink = styled.a`
   color: ${colors.darkGrey};
   font-family: 'Neo Sans Std';
