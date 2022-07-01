4e6b2f7e9ecb00de7d3aa2afecf28dcb96519619
Arthur-Jr <arthurjr1906@gmail.com>
2022-07-01 18:17:40 -0300

    feat: Filter mobile version implement

--- src/components/price-filter/inputs/style.tsx
+++ src/components/price-filter/inputs/style.tsx
@@ -1,14 +1,20 @@
 import styled from 'styled-components';
 
 export const FilterLabel = styled.label`
-  align-items: center;
   display: flex;
-  font-family: 'Lato';
-  font-size: 15px;
+  font-family: Lato;
+  font-size: 18px;
   font-style: normal;
   font-weight: 400;
-  line-height: 17px;
-  padding: 10px 10px 15px 0;
+  padding: 7px;
+
+  @media only screen and (min-width: 1000px) {
+    align-items: center;
+    display: flex;
+    font-size: 15px;
+    line-height: 17px;
+    padding: 10px 10px 15px 0;
+  }
 `;
 
 export const FilterInput = styled.input`
--- src/components/price-filter/style.tsx
+++ src/components/price-filter/style.tsx
@@ -2,9 +2,13 @@ import styled from 'styled-components';
 import colors from '../../styles/colors';
 
 export const FilterWrapper = styled.main`
-  display: flex:
-  flex-direction: column;
-  width: 30%;
+  display: flex;
+  width: 100%;
+
+  @media only screen and (min-width: 1000px) {
+    flex-direction: column;
+    width: auto;
+  }
 `;
 
 export const Title = styled.h4`
@@ -36,16 +40,32 @@ export const SubTitle = styled.h6`
 `;
 
 export const FormSection = styled.section`
-  align-items: space-around;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
+  overflow: scroll;
+  width: 100%;
+
+  @media only screen and (min-width: 1000px) {
+    align-items: space-around;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    overflow: inherit;
+  }
 `;
 
 export const FilterForm = styled.form`
+  display: flex;
+  justify-content: space-around;
+  padding: 7px 6px;
+  width: 850px;
+
+  @media only screen and (min-width: 860px) {
+    width: 950px;
+  }
+
   @media only screen and (min-width: 1000px) {
     align-items: flex-start;
-    display: flex;
     flex-direction: column;
+    justify-content: none;
+    width: 100%;
   }
 `;
