3dd305afbe2677b11dd52723193b9162535472c0
Arthur-Jr <arthurjr1906@gmail.com>
2022-06-30 21:02:24 -0300

    feat: add more colors

--- src/styles/colors.tsx
+++ src/styles/colors.tsx
@@ -1,7 +1,10 @@
 const colors = {
   white: '#FFFFFF',
+  white2: '#fff',
   black: '#111111',
   darkGrey: '#555555',
+  grey: '#666',
+  grey2: '#f5f5f5',
   lightTannat: '#D14B8F',
   lightChardonnay: '#4FBFA5',
 };
