506a21f301e437f521503dcb48f3929ede7601ec
Arthur-Jr <arthurjr1906@gmail.com>
2022-07-01 15:23:00 -0300

    feat: Create filter options object

---
+++ src/utils/filterOptions.tsx
@@ -0,0 +1,13 @@
+interface filterOptions {
+  [key: string]: number[];
+}
+
+const filterOptions: filterOptions = {
+  '1': [40],
+  '2': [40, 60],
+  '3': [100, 200],
+  '4': [200, 500],
+  '5': [500],
+};
+
+export default filterOptions;
