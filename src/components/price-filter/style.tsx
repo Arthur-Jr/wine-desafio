a621e9000df69e0bd9c63b36d0bfb94c9a1d4803
Arthur-Jr <arthurjr1906@gmail.com>
2022-07-01 16:32:13 -0300

    fix: Fix filter sapacements

--- src/components/price-filter/style.tsx
+++ src/components/price-filter/style.tsx
@@ -2,10 +2,8 @@ import styled from 'styled-components';
 import colors from '../../styles/colors';
 
 export const FilterWrapper = styled.main`
-  /* background: red; */
   display: flex:
   flex-direction: column;
-  padding:20px;
   width: 30%;
 `;
 
@@ -14,11 +12,12 @@ export const Title = styled.h4`
 
   @media only screen and (min-width: 1000px) {
     display: inherit;
-    color: ${colors.black2}
+    color: ${colors.black2};
     font-family: Neo Sans Std;
     font-weight: 700;
     font-size: 18px;
-    line-height: 24px
+    line-height: 24px;
+    margin: 0 0 32px;
   }
 `;
 
@@ -32,23 +31,20 @@ export const SubTitle = styled.h6`
     font-weight: 500;
     font-size: 17px;
     line-height: 22px;
-    margin: 0 0 10px;
+    margin: 0 0 16px;
   }
 `;
 
 export const FormSection = styled.section`
   align-items: space-around;
-  /* padding: 20px; */
-  justify-content: space-around;
-  /* background: white; */
   display: flex;
   flex-direction: column;
+  justify-content: space-around;
 `;
 
 export const FilterForm = styled.form`
   @media only screen and (min-width: 1000px) {
     align-items: flex-start;
-    /* background: white; */
     display: flex;
     flex-direction: column;
   }
