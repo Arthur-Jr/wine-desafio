5ccf16e69be4bd26ebafbd2959edf562f30a2def
Arthur-Jr <arthurjr1906@gmail.com>
2022-06-30 21:16:24 -0300

    feat: call TextSearchInput on index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
 import Header from '../components/header/Header';
+import TextSearchInput from '../components/inputs/TextSearchInput';
+import { useAppContext } from '../context/context';
 
 export default function Home() {
+  const { searchStatus } = useAppContext();
+
   return (
     <main>
       <Header />
+      { searchStatus && <TextSearchInput /> }
     </main>
   );
 }
