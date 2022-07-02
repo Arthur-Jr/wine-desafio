13e987827327aaa8e5a0869e238af70eeb8f726d
Arthur-Jr <arthurjr1906@gmail.com>
2022-07-01 22:47:37 -0300

    fix: Add styledComponent on nextConfig

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    // Enables the styled-components SWC transform
-    styledComponents: true
-  }
+  compiler: {
+    styledComponents: true,
+  },
 };
 
 module.exports = nextConfig;
+
+// Referência ao styledComponent no config:
+// https://stackoverflow.com/questions/51791163/warning-prop-classname-did-not-match-when-using-styled-components-with-seman
