af736b7758c372b542b5efef6bcc839d6c325a1d
Arthur-Jr <arthurjr1906@gmail.com>
2022-07-02 21:23:02 -0300

    feat: Filter logic implement

--- src/components/products-section/ProductsSection.tsx
+++ src/components/products-section/ProductsSection.tsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react';
+
 import getAllProducts, { product } from '../../api/getAllProducts';
+import { useAppContext } from '../../context/context';
+import filterByRadio from '../../utils/filterByRadio';
 import ProductCard from './product-card/ProductCard';
 import { CountText, ProductCount, ProductsWrapper, ProductsSectionStyled } from './style';
 
@@ -7,12 +10,28 @@ function ProductsSection() {
   const [allProducts, setAllProducts] = useState<product[]>([]);
   const [productsToDisplay, setProductsToDisplay] = useState<product[]>([]);
 
+  const { textSearchValue, filterValue} = useAppContext();
+
   useEffect((): void => {
     getAllProducts().then((data) => {
       setAllProducts(data);
       setProductsToDisplay(data);
     });
   }, []);
+
+  useEffect((): void => {
+    let toDisplay = allProducts;
+
+    // Filtro do radio input:
+    toDisplay = filterByRadio(filterValue, toDisplay);
+
+    // Filtro do text input:
+    toDisplay = toDisplay.filter(({ name }: { name: string }): boolean => (
+      name.toLowerCase().includes(textSearchValue.toLowerCase()
+    )));
+
+    setProductsToDisplay(toDisplay);
+  }, [allProducts, textSearchValue, filterValue]);
 
   return (
     <ProductsSectionStyled>
