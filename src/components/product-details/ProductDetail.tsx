9e198c66da668a81c25c86f1457c1a5144522f5d
Arthur-Jr <arthurjr1906@gmail.com>
2022-07-03 17:10:49 -0300

    feat: Create product detail component with mobile style

---
+++ src/components/product-details/ProductDetail.tsx
@@ -0,0 +1,88 @@
+import React from 'react';
+
+import { product } from '../../api/getAllProducts';
+import { useAppContext } from '../../context/context';
+import handleAddCartButton from '../../utils/handleAddCartButton';
+import priceToString from '../../utils/priceToString';
+import {
+  CountryFlag,
+  DescriptionText,
+  DescriptionTitle,
+  DescriptionWrapper,
+  DiscountDetail,
+  FullPriceDetail,
+  OriginSpan,
+  OriginWrapper,
+  PriceWrapper,
+  ProductDetailStyled,
+  ProductImage,
+  ProductName,
+  TypeSpan,
+  TypeWrapper,
+  PartnerPriceDetail,
+  NoPartnerPriceDetail,
+  PriceWrapperDetail,
+  DetailButton,
+} from './style';
+
+function ProductDetail({ product }: { product: product }) {
+  const { setCartCountState } = useAppContext();
+
+  return (
+    <>
+      <ProductDetailStyled>
+        <OriginWrapper>
+          <OriginSpan>{'Vinho'}</OriginSpan>
+          <OriginSpan>{'>'}</OriginSpan>
+          <OriginSpan>{product.country}</OriginSpan>
+          <OriginSpan>{'>'}</OriginSpan>
+          <OriginSpan>{product.region}</OriginSpan>
+        </OriginWrapper>
+
+        <ProductName>{product.name}</ProductName>
+
+        <TypeWrapper>
+          <CountryFlag src={product.flag} />
+          <TypeSpan>{product.country}</TypeSpan>
+          <TypeSpan>{product.type}</TypeSpan>
+          <TypeSpan>{product.classification}</TypeSpan>
+          <TypeSpan>{product.volume}</TypeSpan>
+        </TypeWrapper>
+
+        <ProductImage src={product.image}/>
+
+        <DescriptionWrapper>
+          <DescriptionTitle>Descrição</DescriptionTitle>
+          <DescriptionText>{product.sommelierComment}</DescriptionText>
+        </DescriptionWrapper>
+
+      </ProductDetailStyled>
+      <PriceWrapper>
+        <PriceWrapperDetail>
+          <DiscountDetail>{`${product.discount}% OFF`}</DiscountDetail>
+          <FullPriceDetail>{`R$ ${priceToString(product.price)}`}</FullPriceDetail>
+
+          <PartnerPriceDetail>
+            <PartnerPriceDetail>
+              {'R$ '}
+            </PartnerPriceDetail>
+            {`${priceToString(product.priceMember)}`}
+          </PartnerPriceDetail>
+
+          <NoPartnerPriceDetail>
+            {`preço não-sócio R$ ${priceToString(product.priceNonMember)}`}
+          </NoPartnerPriceDetail>
+        </PriceWrapperDetail>
+
+        <DetailButton
+          type="button"
+          onClick={() => handleAddCartButton(product, setCartCountState)}
+        >
+          Adicionar
+        </DetailButton>
+      </PriceWrapper>
+    </>
+  );
+}
+
+export default ProductDetail;
